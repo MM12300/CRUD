e118dcaf0f287c572729e63008bf5f767321d97e
MM12300 <nerggalkore@gmail.com>
2020-04-20 16:14:58 +0200

    finished utilisateur.php

--- utilisateur.php
+++ utilisateur.php
@@ -1,13 +1,19 @@
 <?php
 //On se connecte à la base
 require_once('connect.php');
+
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    $id = $_GET['id'];
+}
+
+
 $sql = 'SELECT * FROM `users` WHERE `id` = :id;';
 
 
 //id est variable, donc on utilise une requête "préparée"
 $query = $db->prepare($sql);
 //On injecte les valeurs dans la requête 
-$query->bindValue(':id', 2, PDO::PARAM_INT);
+$query->bindValue(':id', $id, PDO::PARAM_INT);
 $query->execute();
 
 
